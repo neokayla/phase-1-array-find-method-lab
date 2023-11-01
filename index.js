749354096c7e723f3561b940ccafa7cac82f0177
Kayla Lebron <kayla.lebron12@gmail.com>
2023-10-31 20:10:19 -0400

    hello

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // code your solution here
+const record = [
+    { year: "2015", result: "W"},
+    { year: "2014", result: "N/A"},
+    { year: "2013", result: "L"},
+    //...
+  ]
+
+function superbowlWin(record, result) {
+    const result1 = record.find(({result}) => result === "W");
+    if (result1) {
+        return result1.year;
+    } 
+    else {
+        return undefined;
+    }
+}
+
+
